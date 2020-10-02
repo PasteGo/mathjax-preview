3ea27c77b6d083765799517a8ea9da740180f528
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-10-02 11:05:08 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp       = require("gulp");
+const clean      = require('gulp-clean');
+const concat     = require('gulp-concat');
+const htmlmin    = require('gulp-html-minifier');
+const merge      = require('merge-stream');
+
+function cleanFile(source) {
+    return gulp.src(source)
+               .pipe(clean({force: true}));
+}
+
+function htmlMin(source, destion) {
+	return gulp.src(source)
+		.pipe(htmlmin({
+			removeComments: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			removeEmptyAttributes: true,
+			collapseBooleanAttributes: true,
+			collapseWhitespace: true,
+			minifyJS: true,
+			minifyCSS: true
+		}))
+		.pipe(concat('index.html'))
+		.pipe(gulp.dest(destion));
+}
+
+function gao() {
+    return htmlMin("./src/*.html", "./");
+}
+
+exports.gao = gao;
+exports.default = gulp.series(
+    gao
+);
+
